a2c590cbae85b1201a77fb49c9a36aa137496cb0
Khaled Esam <khaledalabadla6666@gmail.com>
2022-08-03 00:04:48 +0300

    JS ASSIGNMENTS

---
+++ 10-17/Q2.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    console.log(elzero.innerHTML); 
+    console.log(typeof elzero);
+}
---
+++ 10-17/Q4.js
@@ -0,0 +1,5 @@
+let a = 21;
+let b = 20;
+
+console.log("_" + `${a}_${20}`.repeat(4) + "_"); 
+// console.log("_" + `${a}_${b}`.repeat(`${a}${b}`.length) + "_");
