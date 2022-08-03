82f83ca70c6ada2f96938c9d76a7244253f1fa3d
Khaled Esam <khaledalabadla6666@gmail.com>
2022-08-03 18:43:00 +0300

    JS ASSIGNMENTS

---
+++ 18-22/Q2.js
@@ -0,0 +1,25 @@
+let num = 3;
+
+// Solution One
+// console.log(num + num); // 6
+
+// Solution Two
+// console.log(+true + num-- + num); // 6
+
+// Solution Three
+// console.log(num++ + num - true); // 6
+
+// Solution Four
+// console.log(--num + ++num + true); // 6
+
+// Solution Five
+// console.log(--num + ++num + num / num); // 6
+
+// Solution Six
+// console.log(num * num - num); // 6
+
+//solution seven
+// console.log((num ** num - (num * num)) / num);
+
+//solution eight
+// console.log(num * --num)
