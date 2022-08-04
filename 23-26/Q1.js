33000ec99e32170a84b76c23270128ccf51d0abc
Khaled Esam <khaledalabadla6666@gmail.com>
2022-08-04 18:10:51 +0300

    JS ASSIGNMENTS

---
+++ 23-26/Q1.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(10 ** 5); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(1e4 * 10); // 100000
+console.log(1e5); // 100000
+console.log(100000.0); // 100000
+console.log(100e2 * 10); // 100000
+console.log(parseInt("100000")); // 100000
+console.log(7e4 + 3e4); // 100000
+console.log(1e5 * Math.pow(10, 0)); // 100000
+console.log(1e6 / 1e1); // 100000
